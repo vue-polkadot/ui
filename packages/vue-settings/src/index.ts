ff7dd2ef0a81aca912bcccfc832f3a2bd0eefa31
Viktor Valastin <viktorko99@gmail.com>
2019-09-12 17:19:58 +0200

    added vuex setting

---
+++ packages/vue-settings/src/index.ts
@@ -0,0 +1,8 @@
+import SettingModule from './SettingModule'
+// import { Store } from 'vuex'
+
+// export default function install(store: Store<any>) {
+//   store.registerModule('settingModule', SettingModule)
+// }
+
+export default SettingModule
