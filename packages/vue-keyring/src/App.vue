59efa416ef497819e360aa5f1548826ca5e1a822
Matej Nemček <ybdaba@gmail.com>
2019-09-18 10:27:10 +0200

    removing unused things in App.vue

--- packages/vue-keyring/src/App.vue
+++ packages/vue-keyring/src/App.vue
@@ -11,41 +11,25 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-import keyringInstance from './VueKeyring';
+import keyring from './Keyring';
 import { Prefix } from '@polkadot/util-crypto/address/types';
 // import Keypair from './options/Keypair.vue';
-
-// console.log(Keyring);
-@Component({
-  components: {
-    // Keypair
-  }
-})
+@Component({})
 export default class Subkey extends Vue {
   public keyring: any = '';
 
   public mounted(): void {
     console.log('keyring.VUE');
-    this.keyring = keyringInstance.loadAll({
+    this.keyring = keyring.loadAll({
       // genesisHash: 0xdcd1346701ca8396496e52aa2785b1748deb6db09551b72159dcb3e08991025b,
       ss58Format: 42, type: 'sr25519',
       isDevelopment: true });
 
-    console.log(keyringInstance.encodeAddress('0x6674a2958bf589aca9056d57b26f758c50d5aa95aa36dcfbb8659a8bdf7eef6d'));
-    console.log(keyringInstance.decodeAddress('SV4YSr3vJt7nLoE1tMZcpduSNVvvjW5YrPAKUox1BcCr4yZL'));
-    console.log(keyringInstance.getPairs());
+    console.log(keyring.encodeAddress('0x6674a2958bf589aca9056d57b26f758c50d5aa95aa36dcfbb8659a8bdf7eef6d'));
+    console.log(keyring.decodeAddress('SV4YSr3vJt7nLoE1tMZcpduSNVvvjW5YrPAKUox1BcCr4yZL'));
+    console.log(keyring.getPairs());
     // alice
-    // console.log(keyringInstance.getPair('SXYSytZ7wxpQHbRo5FzUFA9wjTfWvTQgYzhVEybWRQvBrvMS'));
+    // console.log(keyring.getPair('SXYSytZ7wxpQHbRo5FzUFA9wjTfWvTQgYzhVEybWRQvBrvMS'));
   }
-
-  // public pair = keyring.getPair('SXYSytZ7wxpQHbRo5FzUFA9wjTfWvTQgYzhVEybWRQvBrvMS');
-  // public isLocked: boolean = this.pair.isLocked;
-  // public meta: object = this.pair.meta;
-  // public saveAccount(pair: object, password: string): void {
-  //   this.keyring.saveAccount(pair, password);
-  // }
-  // public saveAddress(address: string, {...meta}: object): void {
-  //   this.keyring.saveAddress(address, {...meta});
-  // }
 }
 </script>
