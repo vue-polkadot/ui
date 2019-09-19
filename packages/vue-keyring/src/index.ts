c7251529e7b960b0c15919b6155e4e3373f508ec
Matej Nemček <ybdaba@gmail.com>
2019-09-19 10:08:24 +0200

    added assertSingletonPackage for single init

--- packages/vue-keyring/src/index.ts
+++ packages/vue-keyring/src/index.ts
@@ -1,4 +1,8 @@
+import { assertSingletonPackage } from '@polkadot/util';
+
 import keyring, { Keyring } from './Keyring';
+
+assertSingletonPackage('@polkadot/ui-keyring');
 
 export default keyring;
 
