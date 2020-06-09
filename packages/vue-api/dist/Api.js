3ac794191d383a3ba0e84ff7d42544cea75fa964
Viktor Valastin <viktorko99@gmail.com>
2020-06-09 13:05:48 +0200

    Updated vue-api@0.0.19

--- packages/vue-api/dist/Api.js
+++ packages/vue-api/dist/Api.js
@@ -13,71 +13,67 @@ import { getApiOptions } from './utils';
 /**
  * Singleton instance for @polkadot/api.
  */
-let Api = /** @class */ (() => {
-    class Api extends EventEmitter {
-        constructor() {
-            super();
-        }
-        /**
-         * getInstance
-         * @returns Api Instance
-         */
-        static getInstance() {
-            return Api._instance;
-        }
-        /**
-         * connect
-         * @requires apiUrl: string
-         * @returns instance of polkadot-js/api instance
-         */
-        connect(apiUrl) {
-            return __awaiter(this, void 0, void 0, function* () {
-                if (!apiUrl || typeof apiUrl != 'string') {
-                    throw new TypeError(`[VUE API] ERR: Unable to init api with apiUrl ${apiUrl}`);
-                }
-                try {
-                    const provider = new WsProvider(apiUrl);
-                    const options = getApiOptions(apiUrl);
-                    const apiPromise = yield ApiPromise.create(Object.assign({ provider }, options));
-                    this.setApi(apiPromise);
-                    this._emit('connect', apiPromise);
-                }
-                catch (err) {
-                    this._emit('error', err);
-                    throw err;
-                }
-                this.setUrl(apiUrl);
-                return this._api;
-            });
-        }
-        /**
-         * disconnect
-         */
-        disconnect() {
-            if (this._api) {
-                // this._api.once('disconnected', () => this._emit('disconnect', this._apiUrl));
-                this._api.disconnect();
-                this.setUrl(null);
+export default class Api extends EventEmitter {
+    constructor() {
+        super();
+    }
+    /**
+     * getInstance
+     * @returns Api Instance
+     */
+    static getInstance() {
+        return Api._instance;
+    }
+    /**
+     * connect
+     * @requires apiUrl: string
+     * @returns instance of polkadot-js/api instance
+     */
+    connect(apiUrl) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!apiUrl || typeof apiUrl != 'string') {
+                throw new TypeError(`[VUE API] ERR: Unable to init api with apiUrl ${apiUrl}`);
             }
-        }
-        setApi(api) {
-            this._api = api;
-        }
-        setUrl(apiUrl) {
-            this._apiUrl = apiUrl;
-        }
-        get api() {
+            try {
+                const provider = new WsProvider(apiUrl);
+                const options = getApiOptions(apiUrl);
+                const apiPromise = yield ApiPromise.create(Object.assign({ provider }, options));
+                this.setApi(apiPromise);
+                this._emit('connect', apiPromise);
+            }
+            catch (err) {
+                this._emit('error', err);
+                throw err;
+            }
+            this.setUrl(apiUrl);
             return this._api;
-        }
-        /**
-         * tryEmit
-         *
-         */
-        _emit(message = 'event', payload) {
-            this.emit(message, payload);
+        });
+    }
+    /**
+     * disconnect
+     */
+    disconnect() {
+        if (this._api) {
+            // this._api.once('disconnected', () => this._emit('disconnect', this._apiUrl));
+            this._api.disconnect();
+            this.setUrl(null);
         }
     }
-    Api._instance = new Api();
-    return Api;
-})();
-export default Api;
+    setApi(api) {
+        this._api = api;
+    }
+    setUrl(apiUrl) {
+        this._apiUrl = apiUrl;
+    }
+    get api() {
+        return this._api;
+    }
+    /**
+     * tryEmit
+     *
+     */
+    _emit(message = 'event', payload) {
+        this.emit(message, payload);
+    }
+}
+Api._instance = new Api();
