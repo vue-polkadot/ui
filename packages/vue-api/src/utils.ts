635ac43bd778ad19dd17b52471008afd9fb64567
Viki Val <viktorko99@gmail.com>
2021-07-08 09:56:41 +0200

    Updated vue-api@0.0.37

--- packages/vue-api/src/utils.ts
+++ packages/vue-api/src/utils.ts
@@ -1,20 +1,32 @@
 // import { spec as edgewareTypes } from '@edgeware/node-types';
 import usetechTypes from './types/usetech'
+import basiliskTypes from './types/basilisk'
+
 const options = {
   edgeware: {},
   usetech: {
     types: {
       ...usetechTypes
     }
+  },
+  basilisk: {
+    types: {
+      ...basiliskTypes
+    }
   }
 };
 
 const regexes = {
   edgeware: /edgewa/,
-  usetech: /usetech/
+  usetech: /usetech/,
+  basilisk: /basilisk/
 };
 
 export const getApiOptions = (apiUrl: string): any => {
+  if (regexes.basilisk.test(apiUrl)) {
+    return { ...options.basilisk };
+  }
+
   if (regexes.edgeware.test(apiUrl)) {
     return { ...options.edgeware };
   }
