74126fff4038b0cc04901f77e379b5606776953f
Viki Val <viktorko99@gmail.com>
2021-12-09 15:16:56 +0100

    Updated vue-settings@0.0.33

--- packages/vue-settings/src/SettingModule.ts
+++ packages/vue-settings/src/SettingModule.ts
@@ -118,6 +118,12 @@ const SettingModule = {
     },
   },
   getters: {
+    availableUrlPrefixes(state: SettingsStruct): Option[] {
+      return state.avaibleOptions.urlPrefixes
+    },
+    availableIndexers(state: SettingsStruct): Option[] {
+      return state.avaibleOptions.indexers
+    },
     availableNodes(state: SettingsStruct): Option[] {
       return state.avaibleOptions.nodes
     },
@@ -147,12 +153,6 @@ const SettingModule = {
     },
     availableIcons(state: SettingsStruct): Option[] {
       return state.avaibleOptions.icons
-    },
-    availableUrlPrefixes(state: SettingsStruct): Option[] {
-      return state.avaibleOptions.urlPrefixes
-    },
-    availableIndexers(state: SettingsStruct): Option[] {
-      return state.avaibleOptions.indexers
     },
     getSettings({ avaibleOptions, ...rest }: SettingsStruct) {
       return rest
